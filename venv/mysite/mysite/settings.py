d162718b426474e7a4462c957cffde659d3a8d8b
kihyun <ksj57715@hanmail.net>
2020-01-29 22:08:08 +0900

    add redis server

--- venv/mysite/mysite/settings.py
+++ venv/mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!+x!f9pip(qh5t_h98wc#ca=h$o*wlb_ifc0mfzy23wulwhb-d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
